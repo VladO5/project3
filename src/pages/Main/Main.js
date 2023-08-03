e30916e84012f2c4693c37c27efabfe40728b9e3
Vlad Osypchuk <vlad.osipchuk.2017@gmail.com>
2023-08-03 11:48:03 +0300

    update

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,33 @@
-import React from "react";
-import './Main.scss'
+import React, {useState} from "react";
+import './Main.scss';
 
 const Main = () => {
-    return(
-        <div className={'text'}>
-            text from main page
+    const [fileContent, setFileContent] = useState('');
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const content = e.target.result;
+            setFileContent(content);
+        };
+
+        if (file.type === "text/plain" || file.type === "text/csv") {
+            reader.readAsText(file);
+        } else {
+            alert("Підтримуються лише файли типів .txt і .csv");
+        }
+    };
+
+    return (
+        <div className={'text-area'}>
+            <input type="file" className={'waves-effect waves-light btn'} onChange={handleFileChange} />
+            <div id="displayContent">
+                <pre>{fileContent}</pre>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Main;
